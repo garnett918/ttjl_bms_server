a604532a0cea938f1e2338fb434b7bbd21799ae7
garnett918 <garnett_918@126.com>
2017-12-27 20:40:44 +0800

    固定资产开发完成

--- routes/fixedasset.js
+++ routes/fixedasset.js
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
   useConnectionPooling: true
 });
 
-var PAGE_SIZE=10;
+var PAGE_SIZE=5;
 
 
 router.get('/', function(req, res) {
@@ -18,11 +18,33 @@ router.get('/', function(req, res) {
 
     //connection.connect();
 
-    connection.query('SELECT * FROM bim_component', function(err, rows, fields) {
+    var limit = PAGE_SIZE;
+    var offset = PAGE_SIZE*(page-1);
+
+    var sqlstr = 'SELECT * FROM bim_component';
+    if(search !== null || search !== undefined || search !== '')
+    {
+      sqlstr = sqlstr + " WHERE NAME LIKE '%"+search+"%'";
+    }
+
+    sqlstr = sqlstr + ' LIMIT '+offset+','+limit;
+    var countstr = 'SELECT count(*) AS count FROM bim_component';
+    if(search !== null || search !== undefined || search !== '')
+    {
+      countstr = countstr + " WHERE NAME LIKE '%"+search+"%'";
+    }
+    var total_count_all = 0;
+    connection.query(countstr, function(err, rows, fields) {
+      if (err) throw err;
+      total_count_all = rows[0].count;
+      console.log('total_count_all: ', total_count_all);
+    });
+
+    connection.query(sqlstr, function(err, rows, fields) {
       if (err) throw err;
       console.log('The solution is: ', rows[0]);
       console.log('rows.count: ', rows.length);
-      var total_count = rows.length;
+      var total_count = total_count_all;
         var page_count = Math.floor(total_count/PAGE_SIZE);
         if(total_count%PAGE_SIZE>0){
             page_count++;
